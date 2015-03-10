1aedb98e89bd65a19c48be023971336f94f7d074
Dorian Iacobescu <dorian.iacobescu@gmail.com>
2015-03-10 18:09:24 +0200

    add Friendship Model

---
+++ app/models/friendship.rb
@@ -0,0 +1,24 @@
+class Friendship < ActiveRecord::Base
+  belongs_to :user    #the user is the one requesting the friendship
+
+  belongs_to :friend, class_name: "User"  #the friend is the one that receives the request for friendship.
+  # he is still a User, but cannot belong again to User.
+  # this is why a PSEUDOCLASS ":friend" is used.
+  # this will CORELATE the friend_id in Friendship with an actual User
+
+  #handling friendship requests
+  def accept_friendship
+	# "self" is used for the current friendship
+	self.update_attributes(state: "active", friended_at: Time.now)
+  end
+
+  def deny_friendship
+  	self.destroy
+  end
+
+  def cancel_friendship
+  	self.destroy
+  end
+
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,21 @@ class User < ActiveRecord::Base
     # validates_presence_of :username, :etc, :etc
     # validates_uniqueness_of :username, :etc, :etc
 
+
+  # linking with friendship model
+  has_many :friendships, dependent: :destroy # if you delete a user, the corresponding friendships will be deleted
+
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
+  # this user will be the friend_id in the Friendships model, so we can find users that are his friends.
+  # this is why the foreign_key needs to be specified
+
+
+  # requesting friendship
+  # consider user_1 as the user that request the friendship
+  # and user 2 as the one that must accept
+  def request_friendship (user_2)
+    #self will be current user (user_1)
+    self.friendships.create(friend: user_2)
+  end
+
 end
